0267a0d06920ede1f1d57dda52485528cfea9755
M-Usman29 <usmanamir376@gmail.com>
2023-07-09 02:03:30 +0500

    Updated Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 export default function Home(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container" style={{ marginLeft: '30px', fontSize: '18px' ,height:'75px'}}>
+      <div className="container" style={{ marginLeft: '30px', fontSize: '18px', height: '75px', display: 'flex', alignItems: 'center' }}>
         <a className="navbar-brand" href="/">
           <img src="images/logo.png" alt="Logo" height="40" />
           <span style={{ marginLeft: '10px', color: 'white' }}>{props.myname}</span>
@@ -18,48 +18,59 @@ export default function Home(props) {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
-          style={{ order: '-1', marginRight: 'auto' }}
+          style={{ order: '-1', marginRight: 'auto', border: 'none' }}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav" style={{ marginRight: '750px',marginTop:'10px',paddingLeft:'20px' }}>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link text-white" href="#home">
-              {props.home}
+              <a className="nav-link text-white" href="#home" style={{ marginLeft: '20px' }}>
+                {props.home}
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="#portfolio">
-              {props.portfolio}
+              <a className="nav-link text-white" href="#portfolio" style={{ marginLeft: '20px' }}>
+                {props.portfolio}
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="#contact">
-              {props.contact}
+              <a className="nav-link text-white" href="#contact" style={{ marginLeft: '20px' }}>
+                {props.contact}
               </a>
             </li>
           </ul>
         </div>
-        <div style={{ display: 'flex', alignItems: 'center' ,marginLeft:'40px'}}>
-          <FontAwesomeIcon icon={faGlobe} style={{ marginRight: '20px', color: 'orange', transition: 'color 0.5s' }} />
+        <div className="ml-auto" style={{ display: 'flex', alignItems: 'center' }}>
           <button
             className="btn btn-outline-dark"
             style={{
               background: 'linear-gradient(to right, #f83600 0%, #f9d423 100%)',
+              fontSize: '20px',
               color: 'black',
               borderRadius: '20px',
-              padding: '6px 16px',
+              padding: '6px 24px', // Increased button length
               height: 'auto',
               display: 'flex',
               alignItems: 'center',
-              width:'relative',
+              marginLeft: '20px',
+              marginRight: '20px',
             }}
           >
             Contact
           </button>
+          <FontAwesomeIcon icon={faGlobe} style={{ color: 'orange', transition: 'color 0.5s', fontSize: '24px' }} /> {/* Increased globe icon size */}
         </div>
       </div>
+      <style>
+        {`
+          .navbar-nav .nav-link:hover {
+            text-decoration: underline;
+            color: green !important;
+            transform: scale(1.2);
+          }
+        `}
+      </style>
     </nav>
   );
 }
